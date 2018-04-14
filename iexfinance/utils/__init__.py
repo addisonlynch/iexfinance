19b96193ddc099e3dad4211d881ab2bd1e66b788
Aaron Gonzalez <a.gonzalez979@gmail.com>
2018-04-14 10:08:44 -0400

    introduce _sanitize_dates

--- iexfinance/utils/__init__.py
+++ iexfinance/utils/__init__.py
@@ -1,7 +1,34 @@
 import requests
 
+from datetime import datetime
+from pandas import to_datetime
+
 
 def _init_session(session, retry_count=3):
     if session is None:
         session = requests.session()
     return session
+
+
+def _sanitize_dates(start, end):
+    """
+    Return (datetime_start, datetime_end) tuple
+    if start is None - default is 2015/01/01
+    if end is None - default is today
+    """
+    if isinstance(start, int):
+        # regard int as year
+        start = datetime(start, 1, 1)
+    start = to_datetime(start)
+
+    if isinstance(end, int):
+        end = datetime(end, 1, 1)
+    end = to_datetime(end)
+
+    if start is None:
+        start = datetime(2015, 1, 1)
+    if end is None:
+        end = datetime.today()
+    if start > end:
+        raise ValueError('start must be an earlier date than end')
+    return start, end
