33bcafaf6e61373f52424226ec18ea179ef347f2
Boris Musa <boris@kafana.org>
2018-08-22 19:50:12 -0400

    Fixed var name length and error wording
     * Shortened variable name to match project pep8 settings
     * Fixed error message wording

--- iexfinance/__init__.py
+++ iexfinance/__init__.py
@@ -361,7 +361,7 @@ def get_sector_performance(**kwargs):
     data: list
         List of dictionary sector performance items
     """
-    _SECTOR_PERFORMANCE_URL = "https://api.iextrading.com/1.0/stock/market/sector-performance"
+    _URL = "https://api.iextrading.com/1.0/stock/market/sector-performance"
     handler = _IEXBase(**kwargs)
     response = handler._execute_iex_query(_SECTOR_PERFORMANCE_URL)
     if not response:
