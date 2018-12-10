342e2b02e5450df0f0ea6cc5e3ee7ba184e319c7
Addison Lynch <ahlshop@gmail.com>
2018-12-09 20:40:34 -0500

    Add docs code block linting (#92)
    * Added doc flake8 linting with flake8-rst

--- test.sh
+++ test.sh
@@ -2,19 +2,27 @@
 
 echo "flake8 check..."
 flake8 .
-rc=$?; if [[ $rc != 0 ]]; then 
+rc=$?; if [[ $rc != 0 ]]; then
 	echo "flake8 check failed."
-	exit $rc; 
+	exit $rc;
+fi
+echo "PASSED"
+
+echo "flake8-rst docs check..."
+flake8-rst --filename="*.rst" .
+rc=$?; if [[ $rc != 0 ]]; then
+    echo "flake8-rst docs check failed."
+    exit $rc;
 fi
 echo "PASSED"
 
 echo "pytest..."
 pytest -x tests
-rc=$?; 
+rc=$?;
 
-if [[ $rc != 0 ]]; then 
+if [[ $rc != 0 ]]; then
 	echo "Pytest failed."
-	exit $rc 
+	exit $rc
 fi
 echo "PASSED"
 
