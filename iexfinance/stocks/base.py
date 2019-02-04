5955861e1741a65d9f8cbf0e18165f76aa224f46
jackmoody11 <jackmoody@unc.edu>
2019-02-04 16:17:21 +0100

    Update to pass flake8

--- iexfinance/stocks/base.py
+++ iexfinance/stocks/base.py
@@ -158,10 +158,13 @@ class StockReader(_IEXBase):
         ----------
         range: str, default '1m', optional
             Chart range to return. See docs.
-            Choose from ['5y','2y','1y','ytd','6m','3m','1m','1d','date','dynamic']
-            Choosing 'date' will return  IEX-only data by minute for a specified date in 
-            the format YYYYMMDD if available. Currently supporting trailing 30 calendar days.
-            Choosing 'dynamic' will return '1d' or '1m' data depending on the day or week and time of day. 
+            Choose from ['5y','2y','1y','ytd','6m','3m','1m','1d','date',
+            'dynamic']
+            Choosing 'date' will return  IEX-only data by minute for a
+            specified date in the format YYYYMMDD if available.
+            Currently supporting trailing 30 calendar days.
+            Choosing 'dynamic' will return '1d' or '1m' data depending on
+            the day or week and time of day.
             Intraday per minute data is only returned during market hours.
         chartReset: boolean, default True, optional
             If true, 1d chart will reset at midnight instead of the default
@@ -220,8 +223,9 @@ class StockReader(_IEXBase):
         -------
         dict or pandas.DataFrame
             Stocks Delayed Quote endpoint data
-            Includes the following keys/columns: ['symbol', 'delayedPrice', 
-            'delayedSize','delayedPriceTime','processedTime']
+            Includes the following keys/columns: ['symbol',
+            'delayedPrice', 'delayedSize','delayedPriceTime',
+            'processedTime']
         """
         return self._get_endpoint("delayed-quote", params=kwargs)
 
@@ -239,8 +243,9 @@ class StockReader(_IEXBase):
         -------
         list of dicts or pandas.DataFrame
             Stocks Dividends endpoint data
-            Includes the following keys/columns: ['exDate', 'paymentDate', 
-            'recordDate', 'declaredDate', 'amount', 'flag', 'type', 'qualified',
+            Includes the following keys/columns: ['exDate',
+            'paymentDate', 'recordDate', 'declaredDate',
+            'amount', 'flag', 'type', 'qualified',
             'indicated']
         """
         def fmt(out):
@@ -267,9 +272,10 @@ class StockReader(_IEXBase):
         -------
         dict or pandas.DataFrame
             Stocks Earnings endpoint data
-            Includes the following keys/columns: ['actualEPS', 'consensualEPS', 
-            'estimatedEPS', 'announceTime', 'numberOfEstimates', 'EPSSurpriseDollar',
-            'EPSReportDate', 'fiscalPeriod', 'fiscalEndDate', 'yearAgo', 'yearAgoChangePercent',
+            Includes the following keys/columns: ['actualEPS', 'consensualEPS',
+            'estimatedEPS', 'announceTime', 'numberOfEstimates',
+            'EPSSurpriseDollar', 'EPSReportDate', 'fiscalPeriod',
+            'fiscalEndDate', 'yearAgo', 'yearAgoChangePercent',
             'estimatedChangePercent', 'symbolId']
         """
         def fmt(out):
@@ -297,8 +303,9 @@ class StockReader(_IEXBase):
         -------
         list or pandas.DataFrame
             Stocks Effective Spread endpoint data
-            Includes the following keys/columns: ['volume', 'venue', 'venueName', 
-            'effectiveSpread', 'effectiveQuoted', 'priceImprovement']
+            Includes the following keys/columns: ['volume',
+            'venue', 'venueName', 'effectiveSpread',
+            'effectiveQuoted', 'priceImprovement']
         """
         return self._get_endpoint("effective-spread", params=kwargs)
 
@@ -309,16 +316,18 @@ class StockReader(_IEXBase):
         Parameters
         ----------
         period: str, default 'quarter', choose between 'annual' and 'quarter'
-        
+
         Returns
         -------
         dict or pandas.DataFrame
             Stocks Financials endpoint data
             Includes the following keys/columns: ['reportDate', 'grossProfit',
-            'costOfRevenue', 'operatingRevenue', 'totalRevenue', 'operatingIncome', 
-            'netIncome', 'researchAndDevelopment', 'operatingExpense', 'currentAssets', 
-            'totalAsets', totalLiabilities', 'currentCash', 'currentDebt', 'totalCash', 
-            'totalDebt', 'shareholderEquity', 'cashChange', 'cashFlow', 'operatingGainsLosses']
+            'costOfRevenue', 'operatingRevenue', 'totalRevenue',
+            'operatingIncome', 'netIncome', 'researchAndDevelopment',
+            'operatingExpense', 'currentAssets', 'totalAsets',
+            totalLiabilities', 'currentCash', 'currentDebt', 'totalCash',
+            'totalDebt', 'shareholderEquity', 'cashChange', 'cashFlow',
+            'operatingGainsLosses']
         """
         def fmt(out):
             return {symbol: out[symbol].get("financials", []) for symbol in
@@ -346,18 +355,20 @@ class StockReader(_IEXBase):
         -------
         dict or pandas.DataFrame
             Stocks Key Stats endpoint data
-            Includes the following keys/columns: ['companyName', 'marketCap', 
-            'beta', 'week52high', 'week52low', 'shortInterest', 'shortDate', 
-            'dividendRate', 'dividendYield', 'exDividendDate', 'latestEPS', 
-            'latestEPSDate', 'sharesOutstanding', 'float', 'returnOnEquity', 
-            'consensusEPS', 'numberOfEstimates', 'symbol', 'EBITDA', 'revenue', 
-            'grossProfit', 'cash', 'debt', ttmEPS', revenuePerShare', 'revenuePerEmployee', 
-            'peRatioHigh', 'peRatioLow', 'EPSSurpriseDollar', 'EPSSurprisePercent', 
-            'returnOnAssets', 'returnOnCapital', 'profitMargin', 'priceToSales', 'priceToBook', 
-            'day200MovingAvg', 'day50MovingAvg', 'institutionPercent', 'insiderPercent', 
-            'shortRatio', 'year5ChangePercent', 'year2ChangePercent', 'year1ChangePercent', 
-            'ytdChangePercent', 'month6ChangePercent', 'month3ChangePercent', 'month1ChangePercent', 
-            'day5ChangePercent']
+            Includes the following keys/columns: ['companyName', 'marketCap',
+            'beta', 'week52high', 'week52low', 'shortInterest', 'shortDate',
+            'dividendRate', 'dividendYield', 'exDividendDate', 'latestEPS',
+            'latestEPSDate', 'sharesOutstanding', 'float', 'returnOnEquity',
+            'consensusEPS', 'numberOfEstimates', 'symbol', 'EBITDA', 'revenue',
+            'grossProfit', 'cash', 'debt', ttmEPS', revenuePerShare',
+            'revenuePerEmployee', 'peRatioHigh', 'peRatioLow',
+            'EPSSurpriseDollar', 'EPSSurprisePercent', 'returnOnAssets',
+            'returnOnCapital', 'profitMargin', 'priceToSales', 'priceToBook',
+            'day200MovingAvg', 'day50MovingAvg', 'institutionPercent',
+            'insiderPercent', 'shortRatio', 'year5ChangePercent',
+            'year2ChangePercent', 'year1ChangePercent', 'ytdChangePercent',
+            'month6ChangePercent', 'month3ChangePercent', 
+            'month1ChangePercent', 'day5ChangePercent']
         """
         return self._get_endpoint("stats", params=kwargs)
 
