e51ff90b0fd57b28d51f5991cfb34dc99ccf0900
Skyler Arnold <skyarnold1@me.com>
2020-04-18 17:43:04 -0600

    Add trading days/holidays endpoint to refdata (#201)
    * implemented function to get trading/holiday date data
    
    * fix tests i broke
    
    * fix formatting test failures
    
    * add entry to whatsnew file
    
    * fix date/datetime
    
    * woops forgot to remove a debugging print statement
    
    * Repair tests (#205)
    
    * TST: Clean tests, add pauses
    * Working tests
    
    * Simplify code, add documentation
    
    * implemented function to get trading/holiday date data
    
    * fix tests i broke
    
    * fix formatting test failures
    
    * add entry to whatsnew file
    
    * fix date/datetime
    
    * woops forgot to remove a debugging print statement
    
    * Simplify code, add documentation
    
    Co-authored-by: Addison Lynch <ahlshop@gmail.com>

--- iexfinance/refdata/__init__.py
+++ iexfinance/refdata/__init__.py
@@ -1,4 +1,4 @@
-from iexfinance.refdata.base import Symbols, IEXSymbols
+from iexfinance.refdata.base import Symbols, IEXSymbols, TradingDatesReader
 
 
 def get_symbols(**kwargs):
@@ -22,3 +22,30 @@ def get_iex_symbols(**kwargs):
     Data Weighting: ``Free``
     """
     return IEXSymbols(**kwargs).fetch()
+
+
+def get_us_trading_dates_holidays(type_, direction, last=1,
+                                  startDate=None, **kwargs):
+    """
+    Function to obtain US trading dates or holidays from
+    a given date
+
+    Reference: https://iexcloud.io/docs/api/#u-s-holidays-and-trading-dates
+
+    Data Weighting: ``1`` per row returned
+
+    Parameters
+    ----------
+    type_: str
+        can be "trade" or "holiday". Determines whether to return days where
+        trading took place or holidays
+    direction: str
+        can be "next" or "last". Determines whether to return dates in the
+        future or the past
+    last: int, default 1
+        number of days to go backward or forward
+    startDate: str, datetime.datetime, default current date
+        specify first/last day included in next/last, respectively
+    """
+    return TradingDatesReader(type_, direction, last=last,
+                              startDate=startDate, **kwargs).fetch()
--- iexfinance/refdata/base.py
+++ iexfinance/refdata/base.py
@@ -1,4 +1,5 @@
 import datetime
+import pandas as pd
 
 from iexfinance.base import _IEXBase
 
@@ -33,6 +34,36 @@ class CloudRef(_IEXBase):
     @property
     def endpoint(self):
         raise NotImplementedError
+
+
+class TradingDatesReader(CloudRef):
+    """
+    Base class to retrieve trading holiday information
+    """
+    def __init__(self, type_, direction=None, last=1,
+                 startDate=None, **kwargs):
+        if (isinstance(startDate, datetime.date) or
+                isinstance(startDate, datetime.datetime)):
+            self.startDate = startDate.strftime('%Y%m%d')
+        else:
+            self.startDate = startDate
+        self.type = type_
+        if direction not in ("next", "last"):
+            raise ValueError("direction must be either next or last")
+        self.direction = direction
+        self.last = last
+        super(TradingDatesReader, self).__init__(**kwargs)
+
+    @property
+    def endpoint(self):
+        ret = "us/dates/%s/%s/%s" % (self.type, self.direction, self.last)
+        if self.startDate:
+            ret += "/%s" % self.startDate
+        return ret
+
+    def _output_format(self, out, fmt_j=None, fmt_p=None):
+        out = [{k: pd.to_datetime(v) for k, v in day.items()} for day in out]
+        return super()._output_format(out, fmt_j=fmt_j, fmt_p=fmt_p)
 
 
 class Symbols(CloudRef):
